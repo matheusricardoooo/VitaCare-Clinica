51a37af0f44d7b91c862fdb208393886f6e7dc9c
matheusricardoooo <matheusricardodev@gmail.com>
2026-07-31 15:41:16 -0300

    adicionando as importações: connection, driver mananger e sql exception

--- src/main/java/br/com/matheus/vitacare/database/ConexaoBd.java
+++ src/main/java/br/com/matheus/vitacare/database/ConexaoBd.java
@@ -1,4 +1,14 @@
 package br.com.matheus.vitacare.database;
 
+// vai representar a conexão aberta com o banco de dados
+import java.sql.Connection;
+
+// vai encontrar o driver jdbc que o maven baixou
+import java.sql.DriverManager;
+
+// vai realizar o tratamento de exceções caso aconteça
+import java.sql.SQLException;
+
 public class ConexaoBd {
+
 }
