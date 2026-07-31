e96a05a3f43f4f6784edfa7fa425edcd78ecd778
matheusricardoooo <matheusricardodev@gmail.com>
2026-07-31 16:22:13 -0300

    adicionando os imports: properties, inputStream e io exception

--- src/main/java/br/com/matheus/vitacare/database/ConexaoBd.java
+++ src/main/java/br/com/matheus/vitacare/database/ConexaoBd.java
@@ -2,12 +2,16 @@ package br.com.matheus.vitacare.database;
 
 // vai representar a conexão aberta com o banco de dados
 import java.sql.Connection;
-
 // vai encontrar o driver jdbc que o maven baixou
 import java.sql.DriverManager;
-
 // vai realizar o tratamento de exceções caso aconteça
 import java.sql.SQLException;
+// vai ler arquivo de configuração (config.properties)
+import java.util.Properties;
+// cria um fluxo para que o conteúdo do arquivo config.properties possa ser lido.
+import java.io.InputStream;
+// serve para tratamento de erro de leitura do arquivo config
+import java.io.IOException;
 
 public class ConexaoBd {
 
