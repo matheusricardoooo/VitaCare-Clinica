9a4a8ed3558c479d6028c371e27f360f0a41541b
matheusricardoooo <matheusricardodev@gmail.com>
2026-07-31 21:14:49 -0300

    utilizando a class InputStream para abrir o arquivo config e ver os dados contidos no arquivo. Encontrando os dados necessários (url,usuario,senha) é adicionado ao objeto properties, caso não, mensagens de erro são emitidas pelo programa

--- src/main/java/br/com/matheus/vitacare/database/ConexaoBd.java
+++ src/main/java/br/com/matheus/vitacare/database/ConexaoBd.java
@@ -18,5 +18,20 @@ public class ConexaoBd {
     public static Connection getConnection() {
         // objeto criado para guardas as chaves recebidas do config.properties
         Properties properties = new Properties();
+
+        // abre o arquivo config como fluxo de leitura e fecha automaticamente
+        try (InputStream input = ConexaoBd.class.getClassLoader().getResourceAsStream("config.properties")) {
+            // verifica se o arquivo foi encontrado
+            if (input != null) {
+                // se não foi encontrado o programa é interrompido avisando o problema
+                throw new RuntimeException("Arquivo config.properties não encontrado");
+            }
+            // adiciona o conteúdo do config no objeto properties
+            properties.load(input);
+        }
+        // alerta erro de leitura do arquivo
+        catch (IOException e) {
+            throw new RuntimeException("ERRO AO LER O ARQUIVO DE CONFIGURAÇÃO", e);
+        }
     }
 }
