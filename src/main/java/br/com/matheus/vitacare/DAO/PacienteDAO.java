a3dab46c79d76a0ad15e96b82e07b53557856e61
matheusricardoooo <matheusricardodev@gmail.com>
2026-08-05 14:42:51 -0300

    organizando o código de conexão em um try junto com a criação do prepared statement junto com o código para preenchimento das interrogações de values. Criando também o catch, caso a conexão com o banco falhe

--- src/main/java/br/com/matheus/vitacare/DAO/PacienteDAO.java
+++ src/main/java/br/com/matheus/vitacare/DAO/PacienteDAO.java
@@ -4,15 +4,36 @@ import br.com.matheus.vitacare.database.ConexaoBd;
 import br.com.matheus.vitacare.model.Paciente;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Date;
+import java.sql.SQLException;
 
 public class PacienteDAO {
     public void cadastrarPaciente(Paciente paciente) {
-        Connection conexaoBanco = ConexaoBd.getConnection();
         String comandoSql =
                 "INSERT INTO pacientes" +
                         "(nome_paciente, telefone_paciente, cpf_paciente, email_paciente,data_nascimento_paciente)" +
                         "VALUES(?,?,?,?,?)";
 
+        try(
+                Connection conexaoBanco = ConexaoBd.getConnection();
+                PreparedStatement stmt = conexaoBanco.prepareStatement(comandoSql);
 
+        ) {
+            stmt.setString(1, paciente.getNomePaciente());
+            stmt.setString(2, paciente.getTelefonePaciente());
+            stmt.setString(3, paciente.getCpfPaciente());
+            stmt.setString(4, paciente.getEmailPaciente());
+            stmt.setDate(5,Date.valueOf(paciente.getDataNascimentoPaciente()));
+
+            stmt.executeUpdate();
+
+            System.out.println("========================================================");
+            System.out.println("            PACIENTE CADASTRADO COM SUCESSO!            ");
+            System.out.println("========================================================");
+
+        } catch (SQLException e) {
+            throw new RuntimeException("ERRO AO CADASTRAR PACIENTE", e);
+        }
     }
 }
