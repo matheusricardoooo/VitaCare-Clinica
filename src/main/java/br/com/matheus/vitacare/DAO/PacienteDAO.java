b379ba489467c8c5a1a3470b158c771cfe3713ec
matheusricardoooo <matheusricardodev@gmail.com>
2026-08-05 13:39:24 -0300

    importanto as classes de conexão do banco de dados e a classe paciente do package model com os dados e criando o primeiro comando para conexão do banco de dados

--- src/main/java/br/com/matheus/vitacare/DAO/PacienteDAO.java
+++ src/main/java/br/com/matheus/vitacare/DAO/PacienteDAO.java
@@ -1,7 +1,13 @@
 package br.com.matheus.vitacare.DAO;
 
+import br.com.matheus.vitacare.database.ConexaoBd;
+import br.com.matheus.vitacare.model.Paciente;
+
+import java.sql.Connection;
+
 public class PacienteDAO {
     public void cadastrarPaciente(Paciente paciente) {
+        Connection conexaoBanco = ConexaoBd.getConnection();
 
     }
 }
