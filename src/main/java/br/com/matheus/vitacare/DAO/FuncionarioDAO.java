402a0f27f5b0caf5274ab519141593ba925687ba
matheusricardoooo <matheusricardodev@gmail.com>
2026-08-05 22:29:43 -0300

    reaproveitando código da função de buscar pacientes e adaptando para buscar funcionários

--- src/main/java/br/com/matheus/vitacare/DAO/FuncionarioDAO.java
+++ src/main/java/br/com/matheus/vitacare/DAO/FuncionarioDAO.java
@@ -89,4 +89,55 @@ public class FuncionarioDAO {
             throw new RuntimeException("ERRO AO CADASTRAR FUNCIONÁRIO", e);
         }
     }
+
+    public void buscarFuncionarioPorCpf(String cpf) {
+        String buscaNoBanco =
+                "SELECT * FROM funcionarios" +
+                        "WHERE cpf_funcionario = ?";
+
+        try (
+                Connection conexaoBanco = ConexaoBd.getConnection();
+                PreparedStatement stmt = conexaoBanco.prepareStatement(buscaNoBanco);
+        ) {
+
+            stmt.setString(1, cpf);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                int id = rs.getInt("id_funcionario");
+                String nome = rs.getString("nome_funcionario");
+                String telefone = rs.getString("telefone_funcionario");
+                String Cpf = rs.getString("cpf_funcionario");
+                String email = rs.getString("email_funcionario");
+                Date data = rs.getDate("data_nascimento_funcionario");
+                String cargo = rs.getString("cargo_funcionario");
+                Date dataContrato = rs.getDate("data_contratacao");
+
+                System.out.println("=================================================");
+                System.out.println("ID: " + id);
+                System.out.println("------------------------------------");
+                System.out.println("NOME: " + nome);
+                System.out.println("------------------------------------");
+                System.out.println("TELEFONE: " + telefone);
+                System.out.println("------------------------------------");
+                System.out.println("CPF: " + Cpf);
+                System.out.println("------------------------------------");
+                System.out.println("EMAIL: " + email);
+                System.out.println("------------------------------------");
+                System.out.println("DATA NASCIMENTO: " + data);
+                System.out.println("------------------------------------");
+                System.out.println("CARGO: " + cargo);
+                System.out.println("------------------------------------");
+                System.out.println("DATA CONTRATAÇÃO: " + dataContrato);
+                System.out.println("=================================================");
+            }
+            else {
+                System.out.println("==================================================================");
+                System.out.println("        AVISO: NÃO HÁ FUNCIONARIO CADASTRADO COM ESTE CPF         ");
+                System.out.println("==================================================================");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("ERRO AO BUSCAR FUNCIONÁRIO", e);
+        }
+    }
 }
