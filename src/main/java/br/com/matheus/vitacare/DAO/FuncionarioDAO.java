20f744070d69b1307cc6830e0d4e313542fb0769
matheusricardoooo <matheusricardodev@gmail.com>
2026-08-05 21:52:26 -0300

    função de cadastro de funcionários criado

--- src/main/java/br/com/matheus/vitacare/DAO/FuncionarioDAO.java
+++ src/main/java/br/com/matheus/vitacare/DAO/FuncionarioDAO.java
@@ -1,4 +1,9 @@
 package br.com.matheus.vitacare.DAO;
 
+import br.com.matheus.vitacare.model.Funcionario;
+
 public class FuncionarioDAO {
+    public void cadastrarFuncionario(Funcionario funcionario) {
+
+    }
 }
