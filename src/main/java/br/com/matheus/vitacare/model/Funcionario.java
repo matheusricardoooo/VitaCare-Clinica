b6b4b2455aec6b687382d27d28881fae0644b903
matheusricardoooo <matheusricardodev@gmail.com>
2026-08-05 21:07:18 -0300

    retirando o final do atributo de cpf

--- src/main/java/br/com/matheus/vitacare/model/Funcionario.java
+++ src/main/java/br/com/matheus/vitacare/model/Funcionario.java
@@ -6,6 +6,6 @@ public class Funcionario {
     private int id_funcionario;
     private String nome_funcionario;
     private String telefone_funcionario;
-    private String final cpf_funcionario;
+    private String cpf_funcionario;
     private LocalDate data_nascimento_funcionario;
 }
