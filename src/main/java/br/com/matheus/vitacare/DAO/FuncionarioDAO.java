5d9f29269df3bab128e9807ee616333db1ff8041
matheusricardoooo <matheusricardodev@gmail.com>
2026-08-05 22:16:01 -0300

    stmt.set irá adicionar valores no lugar da interrogação de values no comando sql

--- src/main/java/br/com/matheus/vitacare/DAO/FuncionarioDAO.java
+++ src/main/java/br/com/matheus/vitacare/DAO/FuncionarioDAO.java
@@ -4,6 +4,7 @@ import br.com.matheus.vitacare.database.ConexaoBd;
 import br.com.matheus.vitacare.model.Funcionario;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -20,6 +21,20 @@ public class FuncionarioDAO {
                 Connection conexaoBanco = ConexaoBd.getConnection();
                 PreparedStatement stmt = conexaoBanco.prepareStatement(comandoSql);
         ) {
+
+            stmt.setString(1, funcionario.getNomeFuncionario());
+            stmt.setString(2, funcionario.getTelefoneFuncionario());
+            stmt.setString(3, funcionario.getCpfFuncionario());
+            stmt.setString(4, funcionario.getEmailFuncionario());
+            stmt.setDate(5, Date.valueOf(funcionario.getDataNascimentoFuncionario()));
+            stmt.setString(6, funcionario.getCargoFuncionario());
+            stmt.setDate(7, Date.valueOf(funcionario.getDataContratado()));
+
+            stmt.executeUpdate();
+            System.out.println("===========================================================");
+            System.out.println("            FUNCIONÁRIO CADASTRADO COM SUCESSO!            ");
+            System.out.println("===========================================================");
+
         }
         catch (SQLException e) {
             throw new RuntimeException("ERRO AO CADASTRAR FUNCIONÁRIO", e);
