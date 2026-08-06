3b33ff8c61b781f8aab5d38c70a0e03027a2ed0a
matheusricardoooo <matheusricardodev@gmail.com>
2026-08-05 21:53:02 -0300

    criando o comando sql para inserir os dados do funcionário no banco de dados

--- src/main/java/br/com/matheus/vitacare/DAO/FuncionarioDAO.java
+++ src/main/java/br/com/matheus/vitacare/DAO/FuncionarioDAO.java
@@ -4,6 +4,10 @@ import br.com.matheus.vitacare.model.Funcionario;
 
 public class FuncionarioDAO {
     public void cadastrarFuncionario(Funcionario funcionario) {
-
+        String comandoSql =
+                "INSERT INTO funcionarios" +
+                        "nome_funcionario, telefone_funcionario, cpf_funcionario " +
+                        "email_funcionario, data_nascimento_funcionario, cargo_funcionario " +
+                        "data_contratado";
     }
 }
