445537bfb2a3983bf01c8ddf91fde56c45971520
matheusricardoooo <matheusricardodev@gmail.com>
2026-08-05 21:12:49 -0300

    editando nome dos atributos, retirando o "_" e adicionando os atributos: cargoFuncionario e dataContratado

--- src/main/java/br/com/matheus/vitacare/model/Funcionario.java
+++ src/main/java/br/com/matheus/vitacare/model/Funcionario.java
@@ -3,9 +3,11 @@ package br.com.matheus.vitacare.model;
 import java.time.LocalDate;
 
 public class Funcionario {
-    private int id_funcionario;
-    private String nome_funcionario;
-    private String telefone_funcionario;
-    private String cpf_funcionario;
-    private LocalDate data_nascimento_funcionario;
+    private int idFuncionario;
+    private String nomeFuncionario;
+    private String telefoneFuncionario;
+    private String cpfFuncionario;
+    private LocalDate dataNascimentoFuncionario;
+    private String cargoFuncionario;
+    private LocalDate dataContratado;
 }
