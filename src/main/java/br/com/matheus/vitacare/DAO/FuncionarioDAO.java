78d65519e46b1152a96c95e5d0044e405747ad86
matheusricardoooo <matheusricardodev@gmail.com>
2026-08-05 22:02:50 -0300

    criando as tratativas de erro try/catch, no try já está feito a conexão com o banco de dados e a preparação para execução do comando sql

--- src/main/java/br/com/matheus/vitacare/DAO/FuncionarioDAO.java
+++ src/main/java/br/com/matheus/vitacare/DAO/FuncionarioDAO.java
@@ -1,6 +1,11 @@
 package br.com.matheus.vitacare.DAO;
 
+import br.com.matheus.vitacare.database.ConexaoBd;
 import br.com.matheus.vitacare.model.Funcionario;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class FuncionarioDAO {
     public void cadastrarFuncionario(Funcionario funcionario) {
@@ -9,5 +14,14 @@ public class FuncionarioDAO {
                         "nome_funcionario, telefone_funcionario, cpf_funcionario " +
                         "email_funcionario, data_nascimento_funcionario, cargo_funcionario " +
                         "data_contratado";
+
+        try (
+                Connection conexaoBanco = ConexaoBd.getConnection();
+                PreparedStatement stmt = conexaoBanco.prepareStatement(comandoSql);
+        ) {
+
+        } catch (SQLException e) {
+            throw new RuntimeException("ERRO AO CADASTRAR FUNCIONÁRIO", e);
+        }
     }
 }
