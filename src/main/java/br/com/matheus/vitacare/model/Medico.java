2e9353675cbcf2c22b544d04214b69d19c985831
matheusricardoooo <matheusricardodev@gmail.com>
2026-08-07 22:06:11 -0300

    criação dos métodos construtores da classe médico criados

--- src/main/java/br/com/matheus/vitacare/model/Medico.java
+++ src/main/java/br/com/matheus/vitacare/model/Medico.java
@@ -4,4 +4,16 @@ public class Medico {
     private int idFuncionario;
     private String crmMedico;
     private String especialidadeMedica;
+
+    public Medico () {} // construtor vazio para implementação de framework
+
+    public Medico (
+            int idFuncionario,
+            String crmMedico,
+            String especialidadeMedica
+    ) {
+        this.idFuncionario = idFuncionario;
+        this.crmMedico = crmMedico;
+        this.especialidadeMedica = especialidadeMedica
+    }
 }
