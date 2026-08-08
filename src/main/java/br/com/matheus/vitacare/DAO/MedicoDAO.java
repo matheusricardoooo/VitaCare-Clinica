b296b6486b5b0a34709b5b54f8102cf0f3cfbb68
matheusricardoooo <matheusricardodev@gmail.com>
2026-08-07 22:27:08 -0300

    criando o comando sql para inserir dados de novo médico no banco de dados

--- src/main/java/br/com/matheus/vitacare/DAO/MedicoDAO.java
+++ src/main/java/br/com/matheus/vitacare/DAO/MedicoDAO.java
@@ -1,7 +1,16 @@
 package br.com.matheus.vitacare.DAO;
 
+import br.com.matheus.vitacare.database.ConexaoBd;
+import br.com.matheus.vitacare.model.Medico;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MedicoDAO {
     public void cadastrarMedico(Medico medico) {
-
+        String comandoSql =
+                "INSERT INTO medicos" +
+                        "crm_medico, especialidade_medica";
     }
 }
