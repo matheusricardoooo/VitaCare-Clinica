f60ab4304905909c879b44d8be895ebca34b1aa6
matheusricardoooo <matheusricardodev@gmail.com>
2026-08-17 18:33:47 -0300

    construindo o comando sql para inserir uma nova consulta na tabela de consultas do banco de dados

--- src/main/java/br/com/matheus/vitacare/DAO/ConsultaDAO.java
+++ src/main/java/br/com/matheus/vitacare/DAO/ConsultaDAO.java
@@ -4,6 +4,10 @@ import br.com.matheus.vitacare.model.Consulta;
 
 public class ConsultaDAO {
     public void criarConsulta(Consulta consulta) {
-
+        String comandoSql =
+                "INSERT INTO consultas " +
+                        "(id_consulta, id_paciente, id_medico, data_consulta) " +
+                        "(hora_consulta, status_consulta ) " +
+                        "VALUES(?, ?, ?, ?, ?, ?)";
     }
 }
