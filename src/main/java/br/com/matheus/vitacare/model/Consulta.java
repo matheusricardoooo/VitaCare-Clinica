b2582c60ba0223cdfa55275fb67bba3a4dbc4620
matheusricardoooo <matheusricardodev@gmail.com>
2026-08-17 09:09:02 -0300

    declarando os atributos da classe consulta e encapsulando os dados

--- src/main/java/br/com/matheus/vitacare/model/Consulta.java
+++ src/main/java/br/com/matheus/vitacare/model/Consulta.java
@@ -1,4 +1,13 @@
 package br.com.matheus.vitacare.model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Consulta {
+    private int idConsulta;
+    private int idPaciente;
+    private int idMedico;
+    private LocalDate dataConsulta;
+    private LocalTime horaConsulta;
+    private String statusConsulta;
 }
