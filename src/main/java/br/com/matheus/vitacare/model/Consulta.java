d314b66db93b692e2462b3c4a2480af3886a1bb2
matheusricardoooo <matheusricardodev@gmail.com>
2026-08-17 09:16:57 -0300

    método construtor da classe consulta criado

--- src/main/java/br/com/matheus/vitacare/model/Consulta.java
+++ src/main/java/br/com/matheus/vitacare/model/Consulta.java
@@ -10,4 +10,22 @@ public class Consulta {
     private LocalDate dataConsulta;
     private LocalTime horaConsulta;
     private String statusConsulta;
+
+    public Consulta () {} // construtor para ter compatibilidade com framework
+
+    public Consulta ( // construtor incia um objeto atribuindo valor ao atributo declarado
+            int id_consulta,
+            int id_paciente,
+            int id_medico,
+            LocalDate data_consulta,
+            LocalTime hora_consulta,
+            String status_consulta
+    ) {
+        this.idConsulta = id_consulta;
+        this.idPaciente = id_paciente;
+        this.idMedico = id_medico;
+        this.dataConsulta = data_consulta;
+        this.horaConsulta = hora_consulta;
+        this.statusConsulta = status_consulta;
+    }
 }
