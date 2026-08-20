95e739009a2ad03e60f156963739d65421fc641b
matheusricardoooo <matheusricardodev@gmail.com>
2026-08-20 07:57:03 -0300

    criando comando do stmt que irá executar a atualização da data da consulta no banco de dados

--- src/main/java/br/com/matheus/vitacare/DAO/ConsultaDAO.java
+++ src/main/java/br/com/matheus/vitacare/DAO/ConsultaDAO.java
@@ -210,6 +210,20 @@ public class ConsultaDAO {
                 PreparedStatement stmt = conexaoBanco.prepareStatement(atualizacaoBanco);
                 ) {
 
+            stmt.setInt(1,idConsulta);
+            stmt.setDate(2,Date.valueOf(dataAtualizada));
+            int linhasAfetadas = stmt.executeUpdate();
+            if (linhasAfetadas > 0) {
+                System.out.println("============================================================");
+                System.out.println("        SUCESSO: DATA DE CONSULTA ALTERADA COM ÊXITO        ");
+                System.out.println("============================================================");
+            }
+            else {
+                System.out.println("==========================================================");
+                System.out.println("        AVISO: NÃO HÁ NENHUA CONSULTA COM ESTE ID         ");
+                System.out.println("==========================================================");
+            }
+
         } catch (SQLException e) {
             throw new RuntimeException("ERRO AO ATUALIZAR DATA DA CONSULTA", e);
         }
